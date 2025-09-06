8a3b2213416cb7aa5c2ef7aa519cfcee51942a5b
Diego Gerwig <diegogerwig@gmail.com>
2025-09-06 16:14:24 +0200

    update

---
+++ universidad.sql
@@ -0,0 +1,16 @@
+-- ============================================
+-- PROYECTO SQL - BASE DE DATOS UNIVERSIDAD
+-- ============================================
+
+-- ============================================
+-- PASO 1: CREAR Y SELECCIONAR LA BASE DE DATOS
+-- ============================================
+
+-- Eliminar la base de datos si existe (❗CUIDADO❗ esto borra todo)
+DROP DATABASE IF EXISTS universidad;
+
+-- Crear una nueva base de datos
+CREATE DATABASE universidad;
+
+-- Seleccionar la base de datos para trabajar con ella
+USE universidad;
