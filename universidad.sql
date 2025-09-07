99415a9821a6bc86b3a902fa6fa4486f0004d7e8
Diego Gerwig <diegogerwig@gmail.com>
2025-09-07 18:29:32 +0200

    update

--- universidad.sql
+++ universidad.sql
@@ -26,7 +26,7 @@ USE universidad;
 -- ----------------------------
 CREATE TABLE Estudiantes (
     estudiante_id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
+    nombre VARCHAR(100) NOT NULL
 );
 
 -- ----------------------------
@@ -34,7 +34,7 @@ CREATE TABLE Estudiantes (
 -- ----------------------------
 CREATE TABLE Profesores (
     profesor_id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
+    nombre VARCHAR(100) NOT NULL
 );
 
 -- ----------------------------
@@ -44,7 +44,7 @@ CREATE TABLE Asignaturas (
     asignatura_id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     profesor_id INT,
-    FOREIGN KEY (profesor_id) REFERENCES Profesores (profesor_id) ,
+    FOREIGN KEY (profesor_id) REFERENCES Profesores (profesor_id)
 );
 
 -- ----------------------------
@@ -56,5 +56,5 @@ CREATE TABLE Calificaciones (
     asignatura_id INT NOT NULL,
     nota DECIMAL(3, 1),  -- 3 digitos con 1 decimal -> Permite notas del 0.0 al 10.0 
     FOREIGN KEY (estudiante_id) REFERENCES Estudiantes (estudiante_id),
-    FOREIGN KEY (asignatura_id) REFERENCES Asignaturas (asignatura_id),
+    FOREIGN KEY (asignatura_id) REFERENCES Asignaturas (asignatura_id)
 );
